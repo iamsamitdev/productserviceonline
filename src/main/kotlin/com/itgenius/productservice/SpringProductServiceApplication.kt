79443788dfc7606804995947543e1de2075d18b6
Samit Koyom <samitkoyom@gmail.com>
2024-03-07 01:10:24 +0700

    initial commit

---
+++ src/main/kotlin/com/itgenius/productservice/SpringProductServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.itgenius.productservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringProductServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringProductServiceApplication>(*args)
+}
